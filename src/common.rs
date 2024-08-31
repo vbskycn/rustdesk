48ac0f593cdae903d0473f2c44f14798227892b0
vbskycn <34542650+vbskycn@users.noreply.github.com>
2024-08-31 09:31:52 +0800

    Update common.rs
    新增api服务器
    rustdeskapi

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdeskapi.zhoujie218.top".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
